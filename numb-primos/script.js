ad39d0965ac715e8f56a5760bb71b331208323eb
Ezequiel Santos <136353358+ezzysant@users.noreply.github.com>
2024-04-16 17:11:10 -0300

    update optimizando script do projeto numb-primos

--- numb-primos/script.js
+++ numb-primos/script.js
@@ -3,44 +3,31 @@ const btn_verf = document.querySelector('.btn-verificar');//Váriavel do Botão 
 const ele_aviso = document.querySelector('#aviso');//Váriavel do aviso
 const ele_res = document.querySelector('#resposta');//Váriavel para colocar a resposta na div
 
-function numClear(num){//Essa função serve para tirar tudo que não é numero e separar o s numeros com virgula ','
-    let numbers = '';//Váriavel que armazenara os números já limpos
-
-    for(let i = 0; i < num.length;i++){//Laço de repetição
-        if(/[0-9]/.test(num[i])){//Se for numero
-            numbers+=num[i];//Concatenar á váriavel 'numbers'
-        }else if(numbers && numbers[numbers.length - 1] !== ',' && /[0-9]/.test(num[i + 1])){//Se Não, Se a váriavel 'numCler' tiver aldum carácter E o último carácter de 'numbers' não for virgula ',' E o proximo carácter do paramatro 'num' for um número;
-            numbers+=',';//Concatenar a 'numbers'
-        }
-    }
-
-    return numbers;//Retornar 'numbers'
-}
-
 function isPrimo(num){//Essa função verifica se o parametro é um numero primo ou não
     for(let dividir = 2; dividir < num; dividir++){
         if(num%dividir===0)return false;
     }
-    return true
+    return true;
 }
 
 function verifNumPrimo(num){//Essa função aplicara todas as função para verificar apenas numeros e separar os números primos e números não primos;
-    let arrNumbers = [... new Set(numClear(num).split(',').sort((a,b)=> a - b))],//Essa váriavel armasenara o parametro limpo de cáracteres que não são números ou virgula E reorganizara em ordem crecente os numeros com o metodo sort((a,b)=>a-b) E dentro de um objeto Set() removera todos os números repetidos E com o spread operator "..." separara todos os valores do objeto dentro de um Array [ ]
-    objNumbers = {primos:'',noprimos:''};
+    let arrNumbers = [... new Set(num.sort((a,b)=> a - b))],//Essa váriavel armazenara o parametro, com o metodo "sort((a,b)=>a-b)" reorganizara os números em ordem crescente, e isso tudo dentro de um objeto "new Set()" removera todos os números repetidos E com o spread operator "..." separara todos os valores do objeto dentro de um Array [ ].
+    num_primos='',
+    num_noprimos='';
 
     for(let ele of arrNumbers){//Laço de repetição for ..of para verificar cada elemento do Array 'arrNumbers'
         if(Number(ele) > 1 && isPrimo(Number(ele))){//Se o elemento for maior que 1 e For número primo
-            objNumbers.primos += objNumbers.primos ? ','+ele:ele;//Concatenar na propriedade 'primos' do 'objNumbers', Se tiver vazio concatene 'ele' se não concatene " ',' + ele "
+            num_primos += num_primos ? ','+ele:ele;//Concatenar na propriedade 'primos' do 'objNumbers', Se tiver vazio concatene 'ele' se não concatene " ',' + ele "
         }else{//Se não
-            objNumbers.noprimos += objNumbers.noprimos ? ','+ele:ele;//Concatenar na propriedade 'noprimos' do 'objNumbers', Se tiver vazio concatene 'ele' se não concatene " ',' + ele "
+            num_noprimos += num_noprimos ? ','+ele:ele;//Concatenar na propriedade 'noprimos' do 'objNumbers', Se tiver vazio concatene 'ele' se não concatene " ',' + ele "
         }
     }
 
-    if(objNumbers.primos){//Se tem caracteres na propriedade 'primos'
-        ele_res.innerHTML += `Números Primos: ${objNumbers.primos} <br>`;//Concatenar a resposta no 'ele_res'
+    if(num_primos){//Se tem caracteres na propriedade 'primos'
+        ele_res.innerHTML += `Números Primos: ${num_primos} <br>`;//Concatenar a resposta no 'ele_res'
     }
-    if(objNumbers.noprimos){//Se tem caracteres na propriedade 'noprimos'
-        ele_res.innerHTML += `Números Não Primos: ${objNumbers.noprimos}`;//Concatenar a resposta no 'ele_res'
+    if(num_noprimos){//Se tem caracteres na propriedade 'noprimos'
+        ele_res.innerHTML += `Números Não Primos: ${num_noprimos}`;//Concatenar a resposta no 'ele_res'
     }
 
     input_num.value = arrNumbers.join(',');//Aqui ele colocara no input_ele.value os numeros reorganizados;
@@ -48,21 +35,26 @@ function verifNumPrimo(num){//Essa função aplicara todas as função para veri
 
 //Evento de Click no botão .btn-verificar
 btn_verf.addEventListener('click', ()=>{
-    let num = input_num.value;//Váriavel que armazenara
+    let num = input_num.value.match(/[0-9]+/g);//Váriavel que armazenara uma Array, o metodo ".match()" que entre os seus parenteses "()" tem um "RegEx" faz com que apenas números do "value" de "input_num" sejão itens nessa Array que iara ser retornada.
     ele_res.innerHTML = '';//Reiniciando a resposta
     ele_aviso.innerHTML = '';//Reiniciando aviso
-    if(num){
-        verifNumPrimo(num);
+    if(num!== null){//Se "num" for diferente de null
+        verifNumPrimo(num);//Executar funcção
     }
     ele_res.style.visibility = ele_res.innerHTML ? 'visible':'hidden';//Se tever algo escrito no ele_res ele ficara visivel se não ficar escondido
 })
 
+//Evento Input no "input_num"
 input_num.addEventListener('input',()=>{
-    for(let letter of input_num.value){
-        if(!/[0-9,]/.test(letter)){
-            ele_aviso.innerHTML='Caracteres Invalidos';
-            return;
+    for(let letter of input_num.value){//Laço de repetição para todos os caracteres dentro do "value" de "input_num"
+        if(!/[0-9,]/.test(letter)){//Se um dos caracteres não for numero
+            ele_aviso.innerHTML='Caracteres Invalidos';//Mensagem de caracter invalido
+            return;//retornar
         }
     }
+    //Se o laço terminar
+
+    //Se texti dentro do elemento "ele_aviso" estiver preenchido
+    //Ele recebe uma string vazia
     if(ele_aviso.innerHTML)ele_aviso.innerHTML = '';
 })
